cf72140236da4bad171f3e49861d3c31440e9d97
vikramgopali1970 <vikramgopali.93@gmail.com>
2016-10-20 00:24:32 +0530

    Before using controller As

--- public/app/Components/Home/homeController.js
+++ public/app/Components/Home/homeController.js
@@ -6,21 +6,40 @@ app.controller('indexcntrrler', ['$scope','Restaurants', function ($scope, Resta
     $scope.restaurantsListReg = [];
 
     Restaurants.RestaurantsList().then(function (results) {
-        console.log('is it this',results.data);
+        console.log('is it this',results.data.restaurantList);
         $scope.restaurantsListReg = results.data.restaurantList;
         Restaurants.saveRestaurantList(results.data.restaurantList);
     });
+    
+    $scope.dashboard = function () {
+        console.log('dashboard clicked');
+        Restaurants.RestaurantsList().then(function (results) {
+            console.log('is it this',results.data);
+            $scope.restaurantsListReg = results.data.restaurantList;
+            Restaurants.saveRestaurantList(results.data.restaurantList);
+        });
+    };
 
     $scope.setRestaurantIndex = function(index){
         console.log(index);
         Restaurants.selectedRestaurantIndex(index);
         console.log(Restaurants.getSelectedRestaurantIndex());
-    }
+    };
 
     $scope.bookThis = function (bookRestaurant) {
         console.log(bookRestaurant);
         Restaurants.book(bookRestaurant).then(function (results) {
             console.log(results.data);
         })
+    };
+    
+    $scope.applyfilter = function () {
+        console.log($scope.search_query);
+        angular.forEach($scope.restaurantsListReg, function (items) {
+            console.log(typeof(items),items.name);
+            if(items.name.toLowerCase() == $scope.search_query.toLowerCase());
+            $scope.restaurantsListReg.push(items);
+        });
     }
+    
 }]);
